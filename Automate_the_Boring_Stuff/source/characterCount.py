b7e364ce7b79cd4b5e0aabad2d2d4e1bd48996d5
Jarvis <jarvis_su@126.com>
2020-12-28 21:11:25 +0800

    11

--- Automate_the_Boring_Stuff/source/characterCount.py
+++ Automate_the_Boring_Stuff/source/characterCount.py
@@ -1,8 +1,14 @@
-message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.debug('Start of program')
+
+message_str = 'It was a bright cold day in April, and the clocks were striking thirteen.'
 count = {}
 
-for character in message:
+for character in message_str:
     count.setdefault(character, 0)
     count[character] = count[character] + 1
 
 print(count)
+for c in count:
+    logging.debug('Count is ' + c)
